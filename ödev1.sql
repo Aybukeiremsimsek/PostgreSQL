3a9ca3f404fffdeb092a8d7f2307aa048a1c57f9
Aybukeiremsimsek <160397277+Aybukeiremsimsek@users.noreply.github.com>
2025-03-05 22:10:05 +0300

    Update ödev1.sql

--- ödev1.sql
+++ ödev1.sql
@@ -1,22 +1,17 @@
---question1
-select * from employees;
+-- question 1
+select firstname,lastname,salary from employees;
 
 -- question 2
-select firstname from employees;
-select lastname from employees;
-select salary from employees;
+select distinct departmentid from employees;
+--
 
 -- question 3
-select distinct departmentid from employees;
-
--- question 4
 select * from employees
 where departmentid = 1;
 
--- question 5
+-- question 4
 select * from employees
 order by salary desc;
 
--- question 6 
+-- question 5
 select firstname ||':'|| lastname as Fullname from employees;
-
